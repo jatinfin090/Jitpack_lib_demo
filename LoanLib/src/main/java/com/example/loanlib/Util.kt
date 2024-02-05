476d9e00dbc9ebb4d1ebbb02ae2c83f700fd20d5
Keshav Bansal <74243666+pm1-essentialdesigns@users.noreply.github.com>
2024-02-05 11:07:15 +0530

    intial test

---
+++ LoanLib/src/main/java/com/example/loanlib/Util.kt
@@ -0,0 +1,8 @@
+package com.example.loanlib
+
+class Util {
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+}
---
+++ app/src/main/java/com/example/jitpack_lib_demo/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.example.jitpack_lib_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import com.example.loanlib.Util
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       //  Util().sum(1, 2)
+    }
+}
